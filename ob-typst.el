6df88c56cb07fe42c0bec4849326761f62955846
Cj-bc <cj.bc-sd@outlook.jp>
2023-12-21 12:37:28 +0900

    Fix: ob-typst.org-babel-default-header-args:typst> Support inlin src block

--- ob-typst.el
+++ ob-typst.el
@@ -23,16 +23,51 @@
 ;; 
 
 (defvar org-babel-default-header-args:typst
-  '((:results . "file graphics")))
+  '((:results . "file graphics raw"))
+  "Default arguments to use when evaluating a typst source block.
+
+  Having \"raw\" in it enables inline src block outputs only file
+  path, which works nicely with `org-toggle-inline-images'
+  
+  Without it,
+  > src_typst{a/b = abc_d}
+  expands to:
+  > {{{results([[file:/tmp/babel-R4uOI0/org-babel-typsttIVzgp.png]])}}}
+")
+
 
 (defun org-babel-execute:typst (body params)
   "Execute a block of typst code with org-babel.
 This function is called by `org-babel-execute-src-block'."
   (let* ((out-file (or (alist-get :file params)
 		       (org-babel-temp-file "org-babel-typst" ".png"))))
-    (org-embed-typst/create-formula-image body out-file)
+    (ob-typst/create-formula-image body out-file)
     out-file))
 
+(defun ob-typst/create-formula-image (string tofile)
+  "Create an image from typst source using external process.
+
+The Typst STRING is saved to a temporary Typst file, then
+converted to an image file by 'typst compiler' command.
+
+The generated image file is eventually moved to TOFILE.
+
+Generated file format is determined by TOFILE file
+extension. Supported file formats are: .png, .pdf, .svg
+"
+  (let ((tmpfile (make-temp-file "ob-typst"))
+	(ext (file-name-extension tofile))
+	(log-buf (get-buffer-create "*Org Preview typst Output*")))
+    (with-temp-file tmpfile
+      (insert string)
+      (unless (search-backward "#set page(" nil t)
+	(goto-char (point-min))
+	(insert "#set page(width: auto, height: auto, margin: 5pt)\n"))
+      )
+    (copy-file (org-compile-file tmpfile
+				 (list (format "typst compile --format %s --root %%o %%f" ext))
+				 ext "" log-buf)
+	       tofile 'replace)))
 ;;; Code:
 
 
